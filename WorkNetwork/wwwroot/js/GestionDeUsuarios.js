4c4799194fe9111ba0a0fb815a2ed319be49a1b8
Agustin <agucomba@gmail.com>
2022-11-16 23:30:34 -0300

    Pagina principal finalizada

--- WorkNetwork/wwwroot/js/GestionDeUsuarios.js
+++ WorkNetwork/wwwroot/js/GestionDeUsuarios.js
@@ -1,65 +1,86 @@
-﻿function login(){	
-	event.preventDefault();
-	let correo =	$('#loginCorreo').val();
-	let pass = $('#loginPass').val();
-	let url = '../../GestionDeUsuarios/Ingresar';
-	let data = { email: correo, password:pass};
-	$.post(url, data).done(resultado => {
-		resultado ? window.location.href='/' : alert('usuario o contraseña incorrecta')
-	}).fail(e => console.log(e))
+﻿////function login(){	
+////	event.preventDefault();
+////	let correo =	$('#loginCorreo').val();
+////	let pass = $('#loginPass').val();
+////	let url = '../../GestionDeUsuarios/Ingresar';
+////	let data = { email: correo, password:pass};
+////	$.post(url, data).done(resultado => {
+////		resultado ? window.location.href='/' : alert('usuario o contraseña incorrecta')
+////	}).fail(e => console.log(e))
+////}
+
+function vaciarForm() {
+    $("#loginCorreo").val("");
+    $("#loginPass").val("");
+    $('#loginError').addClass('visually-hidden');
 }
 
-function registerPerson(){
-	event.preventDefault();
-	let rol = $('#rol').val()
-	let correo= $('#correo').val()
-	let pass= $('#pass').val();
-	let passConfirm= $('#passRepeat').val();
-	//$("#registrarPersona").addClass('visually-hidden')
-	//$('#loaderRegisterPersona').removeClass('visually-hidden')
-	let url = '../../GestionDeUsuarios/Registrar';
-	let data = { email: correo, password: pass, Rol: rol }
-	if(pass === passConfirm){
-		$.post(url,data).done(
-			(resultado)=>{
-				if (resultado) {
-					window.location.href = '/'
-				}else{
-					$('#errorRegisterPersona').text('El usuario ya existe')
-					$('#errorRegisterPersona').removeClass('visually-hidden');
-					$('#loaderRegisterPersona').addClass('visually-hidden');
-					$("#registrarPersona").removeClass('visually-hidden');
-				}
-			}
-		)
-	}else{
-		$('#errorRegisterPersona').text('Las contraseñas no coinciden')
-		$('#errorRegisterPersona').removeClass('visually-hidden');
-		$('#loaderRegisterPersona').addClass('visually-hidden');
-		$("#registrarPersona").removeClass('visually-hidden');
-	}
+function registerPerson() {
+    event.preventDefault();
+    let rol = $('#rol').val();
+    let correo = $('#correo').val().trim();
+    let pass = $('#pass').val();
+    let passConfirm = $('#passRepeat').val();
+    $("#registerBtn").addClass("visually-hidden")
+    $('#registerLoader').removeClass("visually-hidden")
+    let url = '../../GestionDeUsuarios/Registrar';
+    let data = { email: correo, password: pass, Rol: rol }
+    if (rol) {
+        if (correo) {
+            if (pass === passConfirm && pass !== '') {
+                $.post(url, data).done(
+                    (resultado) => {
+                        if (resultado) {
+                            window.location.href = '/'
+                        } else {
+                            $('#registerError').text('El usuario ya existe')
+                            $('#registerError').removeClass("visually-hidden");
+                            $('#registerLoader').addClass("visually-hidden");
+                            $("#registerBtn").removeClass("visually-hidden");
+                        }
+                    }
+                )
+            } else {
+                $('#registerError').text('Las contraseñas no coinciden')
+                $('#registerError').removeClass("visually-hidden");
+                $('#registerLoader').addClass("visually-hidden");
+                $("#registerBtn").removeClass("visually-hidden");
+            }
+        } else {
+            $('#registerError').text('Debe ingresar un correo')
+            $('#registerError').removeClass("visually-hidden");
+            $('#registerLoader').addClass("visually-hidden");
+            $("#registerBtn").removeClass("visually-hidden");
+        }
+
+    } else {
+        $('#registerError').text('Debe seleccionar un rol')
+        $('#registerError').removeClass("visually-hidden");
+        $('#registerLoader').addClass("visually-hidden");
+        $("#registerBtn").removeClass("visually-hidden");
+    }
 
 }
-function loginPersona(){	
-	event.preventDefault();
-	let correoPersona=	$('#correoPersona').val();
-	let passPersona= $('#passwordPersona').val();
-	let url = '../../GestionDeUsuarios/Ingresar';
-	let loader = document.getElementById('loaderLoginPersona') 
-	$('#iniciarSesionPersona').addClass('visually-hidden');
-	$('#loaderLoginPersona').removeClass('visually-hidden');
-	let data = { email: correoPersona, password:passPersona};
-	$.post(url, data).done(resultado => {
-		if(resultado){
-			window.location.href='/'
-		}else{
-			loader.classList.add('visually-hidden');
-			$('#errorInicioSesion').removeClass('visually-hidden');
-			$('#iniciarSesionPersona').removeClass('visually-hidden')
-		}
-	}).fail(e => {
-		$('#iniciarSesionPersona').removeClass('visually-hidden')
-		$('#errorInicioSesion').removeClass('visually-hidden')
-		loader.classList.add('visually-hidden')	
-	})
+
+function login() {
+    event.preventDefault();
+    let correo = $('#loginCorreo').val();
+    let pass = $('#loginPass').val();
+    let url = '../../GestionDeUsuarios/Ingresar';
+    let data = { email: correo, password: pass };
+    $('#loginBtn').addClass('visually-hidden');
+    $('#loginLoader').removeClass('visually-hidden');
+    $.post(url, data).done(resultado => {
+        if (resultado) {
+            window.location.href = '/'
+        } else {
+            $("#loginLoader").addClass('visually-hidden');
+            $('#loginError').removeClass('visually-hidden');
+            $('#loginBtn').removeClass('visually-hidden')
+        }
+    }).fail(e => {
+        $('#loginBtn').removeClass('visually-hidden')
+        $('#loginError').removeClass('visually-hidden')
+        $("#loginLoader").addClass('visually-hidden');
+    })
 }
